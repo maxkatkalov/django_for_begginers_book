f8a77ede122c523af4d05bd321643226a9ff7612
Max Katkalov <makskatkalov@gmail.com>
2023-07-11 17:47:03 +0300

    db settings added

--- django_project/settings.py
+++ django_project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'techone1_test',
+        "PASSWORD": "UIaw*JpS~uqC",
+        "HOST": "localhost",
+        "PORT": "3306"
     }
 }
 
