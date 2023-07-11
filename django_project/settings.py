70caab85b0056202088962699d1778a101837c5a
Max Katkalov <makskatkalov@gmail.com>
2023-07-11 18:20:02 +0300

    changed db to sqlLite

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'techone1_test',
-        "USER": "techone1_test",
-        "PASSWORD": "UIaw*JpS~uqC",
-        "HOST": "localhost",
-        "PORT": "3306"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
